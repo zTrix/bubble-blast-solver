26d93acbdfdf1272762429505d980764aeb1bba0
zTrix <i@ztrix.me>
2013-01-18 17:21:58 +0800

    add simu elimination, need adjustment

--- solve.py
+++ solve.py
@@ -11,15 +11,45 @@ def iszero(mat):
                 return False
     return True
 
-directions = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)]
+directions = [(0, 0), (-1, 0), (0, -1), (1, 0), (0, 1)]
 
-bug_logic_open = False
+# example: eliminate_simu(mat, [2, 3])
+def eliminate_simu(mat, point):
+    minis = [[point[0], point[1], 1]]
+
+    rows = len(mat)
+    cols = len(mat[0])
+
+    def add(i, j, s):
+        if i < rows and i >= 0 and j < cols and j >= 0:
+            minis.append([i, j, s])
+
+    while len(minis):
+        total = len(minis)
+        for i in range(total):
+            if mat[minis[i][0]][minis[i][1]]:
+                mat[minis[i][0]][minis[i][1]] -= 1
+                minis[i][2] = 0
+                if mat[minis[i][0]][minis[i][1]] == 0:
+                    add(minis[i][0]-1, minis[i][1], 1)
+                    add(minis[i][0], minis[i][1]-1, 2)
+                    add(minis[i][0]+1, minis[i][1], 3)
+                    add(minis[i][0], minis[i][1]+1, 4)
+            else:
+                add(minis[i][0] + directions[minis[i][2]][0], minis[i][1] + directions[minis[i][2]][1], minis[i][2])
+                minis[i][2] = 0
+        total = len(minis) - 1
+        for i in range(total, -1, -1):
+            if minis[i][2] == 0:
+                minis[i] = minis[len(minis)-1]
+                minis.pop()
+
+## example here:
+# op = [[0 for _j in range(cols)] for _i in range(rows)]
+# op[i][j] = [(1,0)]
+# eliminate(mat, op)
 
 def eliminate(mat, ops):
-    #print '\n', ops
-    #for i in mat:
-    #    print i
-    #raw_input("\ncontinue ...\n")
     rows = len(mat)
     cols = len(mat[0])
     new_ops = [[[] for j in range(cols)] for i in range(rows)]
@@ -43,26 +73,10 @@ def eliminate(mat, ops):
                         mat[i][j] -= len(ops[i][j])
                     else:
                         flag += add(i+1, j, 3, 1)
-                        flag += add(i-1, j, 4, 1)
-                        flag += add(i, j+1, 1, 1)
+                        flag += add(i-1, j, 1, 1)
+                        flag += add(i, j+1, 4, 1)
                         flag += add(i, j-1, 2, 1)
                         mat[i][j] = 0
-                        if not bug_logic_open:
-                            continue        
-                        # the following logic deals with bubble blast bug, if needed
-                        if mat[i][j] == len(ops[i][j]):
-                            mat[i][j] = 0
-                            continue
-                        ops[i][j].sort(cmp = cmp)
-                        k = mat[i][j]
-                        mat[i][j] = 0
-                        mind = ops[i][j][k-1][1]
-                        while k < len(ops[i][j]):
-                            if ops[i][j][k][1] == mind:
-                                k += 1
-                                continue
-                            flag += add(i + directions[ops[i][j][k][0]][0], j + directions[ops[i][j][k][0]][1], ops[i][j][k][0], ops[i][j][k][1]+1)
-                            k += 1
                 else:
                     for b, d in ops[i][j]:
                         flag += add(i + directions[b][0], j + directions[b][1], b, d+1)
@@ -117,9 +131,7 @@ def quick_solve(mat, remain):
             nonzero = True
             ans[remain] = (i,j)
             nm = copy.deepcopy(mat)
-            op = [[0 for _j in range(cols)] for _i in range(rows)]
-            op[i][j] = [(1,0)]
-            eliminate(nm, op)
+            eliminate_simu(nm, [i, j])
             if solve(nm, remain-1): return True
     return not nonzero
 
@@ -146,14 +158,6 @@ def main(filepath, cnt):
             print ans[i]
         return 0
     print >> sys.stderr, 'no solution under normal logic(%.2fs), trying brute force with buggy elimination...' % (time.time() - start_time)
-    start_time = time.time()
-    bug_logic_open = True
-    if solve(mat, cnt):
-        print >> sys.stderr, 'found solution in %.2fs using brute force buggy elimination' % (time.time() - start_time)
-        for i in range(cnt, 0, -1):
-            print ans[i]
-        return 0
-    print >> sys.stderr, 'cannot find solution (%.2fs)' % (time.time() - start_time)
     return 11
 
 if __name__ == '__main__':
